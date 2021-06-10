f66b5f97eb2cb45620409c2e8e91b1f123e6ebd8
GokhanT <tekirgokhan74@gmail.com>
2021-06-10 23:58:53 +0300

    update TYT_MAT.py

---
+++ TYT_MAT.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+list_x = [0,1,2,3,4,5,6]
+list_y = [33.75,32.75,32.0,30.75,33.25,32.5,33.0]
+
+plt.axhline(y = 40,linewidth = 1,color = 'r',label = "Max Net")
+plt.xlabel("Deneme Sayısı")
+plt.ylabel("Net")
+plt.legend(['Hedef'])
+plt.title("TYT MAT DENEME")
+plt.grid(True)
+plt.plot(list_x,list_y,'ro')
+
+plt.show()
